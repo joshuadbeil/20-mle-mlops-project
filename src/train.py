2e50a5a0a382fa54a3a00ac358efa92d1269d0f6
Joshua Beilenhoff <j.beilenhoff@gmail.com>
2023-07-23 13:14:21 +0200

    train script for git action

--- src/train.py
+++ src/train.py
@@ -1,16 +1,86 @@
 import os
 import argparse
-import base64
 import pandas as pd
 import mlflow
+from mlflow.tracking.client import MlflowClient
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
-from sklearn.pipeline import make_pipeline
+from train_preprocess import LrTaxiTransformer
 
+# Allow for argument from CLI call
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "--cml_run", default=False, action=argparse.BooleanOptionalAction, required=True
 )
 args = parser.parse_args()
 cml_run = args.cml_run
+
+# Set variables
+color = "green"
+year = 2021
+month = 1
+model_name = "green-taxi-trip-duration-linr"
+
+# set credentials for MLFlow and DVC
+GOOGLE_APPLICATION_CREDENTIALS = "./credentials.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = GOOGLE_APPLICATION_CREDENTIALS
+
+# set MLFlow experiment environment
+MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
+mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+mlflow.set_experiment(model_name)
+client = MlflowClient(tracking_uri=MLFLOW_TRACKING_URI)
+
+# get data
+df = pd.read_parquet(f"./data/{color}_tripdata_{year}-{month:02d}.parquet")
+
+#transform data
+transformer = LrTaxiTransformer()
+X, y = transformer.fit_transform(df)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.2)
+
+
+with mlflow.start_run() as run:
+    
+    tags = {
+        "model": "linear regression",
+        "developer": "joshuadbeil",
+        "dataset": f"{color}-taxi",
+        "year": year,
+        "month": month,
+        "features": transformer.features,
+        "target": transformer.target
+    }
+    mlflow.set_tags(tags)
+    
+    lr = LinearRegression()
+    lr.fit(X_train, y_train)
+
+    y_pred_train = lr.predict(X_train)
+    rmse_train = mean_squared_error(y_train, y_pred_train, squared=False)
+    mlflow.log_metric("rmse train", rmse_train)
+
+    y_pred_test = lr.predict(X_test)
+    rmse_test = mean_squared_error(y_test, y_pred_test, squared=False)
+    mlflow.log_metric("rmse test", rmse_test)
+    
+    mlflow.sklearn.log_model(lr, "model")
+
+    run_id = run.info.run_id
+    model_uri = f"runs:/{run_id}/model"
+    mlflow.register_model(model_uri=model_uri, name=model_name)
+
+    model_version = 1
+    new_stage = "Production"
+    client.transition_model_version_stage(
+        name=model_name,
+        version=model_version,
+        stage=new_stage,
+        archive_existing_versions=True
+    )
+
+if cml_run:
+    with open("metrics.txt", "w") as f:
+        f.write(f"RMSE on the Train Set: {rmse_train}")
+        f.write(f"RMSE on the Test Set: {rmse_test}")
---
+++ src/train_preprocess.py
@@ -0,0 +1,31 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+import pandas as pd
+import numpy as np
+
+class LrTaxiTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.features = ["PULocationID", "DOLocationID", "trip_distance", "passenger_count", "fare_amount", "total_amount"]
+        self.target = 'trip_duration_minutes'
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, data):
+        data = data.copy()
+        data = self.calculate_and_filter_trip_duration_in_minutes(data)
+        data = self.limit_passenger_count(data)
+        
+        y = data[self.target]
+        X = data[self.features]
+        return X, y
+
+    def calculate_and_filter_trip_duration_in_minutes(self, data):
+        data["trip_duration_minutes"] = (
+            data["lpep_dropoff_datetime"] - data["lpep_pickup_datetime"]
+            ).dt.total_seconds() / 60
+        data = data[(data["trip_duration_minutes"] >= 1) & (data["trip_duration_minutes"] <= 60)]
+        return data
+    
+    def limit_passenger_count(self, data):
+        data = data[(data['passenger_count'] > 0) & (data['passenger_count'] < 8)]
+        return data
